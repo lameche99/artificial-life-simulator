5b9896cf31517b2bcbbabc783bcccbc46f087562
kichappa <kichappa@gmail.com>
2023-10-27 13:13:29 -0400

    cellular automata basic agent modeling

---
+++ test.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 78a9b5b2-7350-11ee-0004-f1f380c2d92c
+
+
+# ╔═╡ Cell order:
+# ╠═78a9b5b2-7350-11ee-0004-f1f380c2d92c
