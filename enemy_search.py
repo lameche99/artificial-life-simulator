d38eabc28937e91663a9fc81fb537d69ac4eab11
lameche99 <85850159+lameche99@users.noreply.github.com>
2023-12-01 20:23:06 -0500

    handling multiple agents logic

--- enemy_search.py
+++ enemy_search.py
@@ -15,7 +15,7 @@ class Agent:
         self.view = view_sight
         self.gather = gather_sight
         self.env_len = environment_len
-        self.discovered_enemy = None
+        self.discovered_enemy = list()
         self.enemies_seen = list()
         self.bushes = list()
         self.surr_field = None
@@ -64,10 +64,8 @@ class Agent:
                     continue
                 if self.surr_field[i][j] == 'enemy': # change with enemy label in environment
                     if (i,j) not in self.enemies_seen:
-                        self.discovered_enemy = (i,j) # set new discovered enemy for strategic search
                         self.enemies_seen.append((i,j)) # mark as seen
-                    else:
-                        self.discovered_enemy = (i,j) # set discovered enemy already seen
+                    self.discovered_enemy.append(i,j) # set discovered enemy already seen
 
     def scan_surrounding(self):
         # Implement function to get surrounging information from the Environemt Class for bushes and enemies present
@@ -101,18 +99,44 @@ class SearchModel:
 
             agent.scan_surrounding()
 
-            if agent.discovered_enemy: 
-                # need to handle case if there are multiple enemies in the agent's field of vision
-                if agent.discovered_enemy in agent.enemies_seen:
-                    # still do random search if enemy has already been seen?
-                    # maybe just strategic search again
-                    new_position = agent.random_search()
-                else:
-                    # Implement avoid_enemy_search when a new enemy is detected
-                    new_position = agent.strategic_search(agent.discovered_enemy[0], agent.discovered_enemy[1])
-            else:
-                # Implement random_search when no enemy is detected
+            bushes = agent.bushes # list of bush coordinates in field of vision
+            enemies = agent.discovered_enemy # list of enemy coordinates in current field of vision
+            seen = len(enemies)
+
+
+            if seen > 1: # if multiple enemies in the field of vision
+                # remove already seen enemies from list to focus on new ones
+                for i in range(seen):
+                    if enemies[i] in agent.enemies_seen:
+                        enemies.remove(enemies[i])
+                if (seen - len(enemies)) == 0: # already seen every enemy
+                    pass # discuss how to handle
+                # if there are non-detected enemies implement strategic search for the first in the list
+                # regardless of how many there are
+                new_position = agent.strategic_search(enemies[0][0], enemies[0][1])
+            
+            elif seen == 1: # only one enemy
+                if enemies[0] in agent.enemies_seen:
+                    # if enemy has already been detected what to do?
+                    pass
+                # if the enemy was not detected already implement strategic search
+                new_position = agent.strategic_search(enemies[0][0], enemies[0][1])
+            
+            else: # no enemies detected perform random search
                 new_position = agent.random_search()
+                        
+            # if agent.discovered_enemy: 
+            #     # need to handle case if there are multiple enemies in the agent's field of vision
+            #     if agent.discovered_enemy in agent.enemies_seen:
+            #         # still do random search if enemy has already been seen?
+            #         # maybe just strategic search again
+            #         new_position = agent.random_search()
+            #     else:
+            #         # Implement avoid_enemy_search when a new enemy is detected
+            #         new_position = agent.strategic_search(agent.discovered_enemy[0], agent.discovered_enemy[1])
+            # else:
+            #     # Implement random_search when no enemy is detected
+            #     new_position = agent.random_search()
 
             agent.x, agent.y = new_position
 
